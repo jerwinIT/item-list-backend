a07aeacb2050682aab1541ca22525788eed46549
Aeron James A. Castillo <aeronjamescastillo6@gmail.com>
2025-03-07 21:52:15 +0800

    update

---
+++ controller/read.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import {getItems} from "/models/queries.js";
+
+const app = express();
+
+app.get("/controller/create.js", (req, res) => {
+    // User Device Data
+    console.log(req.rawHeaders);
+    
+    // Server Response
+    res.json(getItems());
+})
